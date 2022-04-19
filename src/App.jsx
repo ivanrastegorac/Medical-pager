2b83949976a2a7e271a5bd5ef3a6ff5ffee48c09
Ivan Raštegorac <ivan.rastegorac@gmail.com>
2022-04-19 15:22:03 +0200

    add SignIn and SignUp in Auth.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
-import { ChannelContainer, ChannelListContainer } from './components';
+import { ChannelContainer, ChannelListContainer, Auth } from './components';
 import './App.css';
 
 const apiKey = '9yagw2n6bfy3';
 
 const client = StreamChat.getInstance(apiKey);
 
+const authToken = false;
+
 const App = () => {
+  if (!authToken) return <Auth />;
+
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
